22d441016217769c9ea378fcc3028ccd1561ed54
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-09-13 17:12:58 -0500

    feat: Add initializer

--- TaskMaster/Model/TaskListCardModel.swift
+++ TaskMaster/Model/TaskListCardModel.swift
@@ -7,5 +7,28 @@
 
 class TaskListCardModel
 {
+  private var taskGroupId : String
+  private var taskListId : String
+  private var cardId : String
+  private var taskIndex : Int
+  private var cardTitle : String
+  private var cardDetailedDescription : String
+  private var cardIndex : Int
   
+  init(_ taskGroupId : String,
+       _ taskListId : String,
+       _ cardId : String,
+       _ taskIndex : Int,
+       _ cardTitle : String,
+       _ cardDetailedDescription : String,
+       _ cardIndex : Int)
+  {
+    self.taskGroupId = taskGroupId
+    self.taskListId = taskListId
+    self.cardId = cardId
+    self.taskIndex = taskIndex
+    self.cardTitle = cardTitle
+    self.cardDetailedDescription = cardDetailedDescription
+    self.cardIndex = cardIndex
+  }
 }
