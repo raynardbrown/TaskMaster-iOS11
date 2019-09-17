068c25263001782821183cedbdadd98fc0029d45
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-09-16 23:40:38 -0500

    feat: Add the MainViewController class stub

---
+++ TaskMaster/Controller/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//
+//  Created by Raynard Brown on 2019-09-16.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController : UIViewController
+{
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+}
