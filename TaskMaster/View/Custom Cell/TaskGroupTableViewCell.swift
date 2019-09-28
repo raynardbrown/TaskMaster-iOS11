fe176bb89bcc72b80ab36b7e15085c3a5916a2f8
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-09-27 23:59:19 -0500

    feat: Add TaskGroupTableViewCell class

---
+++ TaskMaster/View/Custom Cell/TaskGroupTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskGroupTableViewCell.swift
+//
+//  Created by Raynard Brown on 2019-09-26.
+//  Copyright © 2019 Raynard Brown. All rights reserved.
+//
+
+import UIKit
+
+class TaskGroupTableViewCell : UITableViewCell
+{
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+    // Initialization code
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool)
+  {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+  }
+}
